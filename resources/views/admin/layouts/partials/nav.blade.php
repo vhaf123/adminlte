9914bc3609bf8ec5bae97b521f566d62f2e5bc07
vhaf123 <victor.aranaf92@gmail.com>
2020-06-14 23:41:48 -0500

    modificar avatar tamaño

--- resources/views/admin/layouts/partials/nav.blade.php
+++ resources/views/admin/layouts/partials/nav.blade.php
@@ -119,21 +119,21 @@
 
         <li class="nav-item dropdown user-menu">
             <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
-            <img src="{{asset('adminlte/dist/img/user2-160x160.jpg')}}" class="user-image img-circle elevation-2" alt="User Image">
+            <img src="{{auth()->user()->avatar}}" class="user-image img-circle elevation-2" alt="User Image">
             <span class="d-none d-md-inline">{{ucwords(auth()->user()->name)}}</span>
             </a>
             <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
             <!-- User image -->
             <li class="user-header bg-info">
-                <img src="{{asset('adminlte/dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
+                <img src="{{auth()->user()->avatar}}" class="img-circle elevation-2" alt="User Image">
 
                 <p>
-                    {{ucwords(auth()->user()->name)}} - Web Developer
-                    <small>Member since Nov. 2012</small>
+                    {{ucwords(auth()->user()->name)}}
+                    <small>Miembro desde {{auth()->user()->created_at->diffForHumans()}}</small>
                 </p>
             </li>
             <!-- Menu Body -->
-            <li class="user-body">
+            {{-- <li class="user-body">
                 <div class="row">
                 <div class="col-4 text-center">
                     <a href="#">Followers</a>
@@ -145,8 +145,7 @@
                     <a href="#">Friends</a>
                 </div>
                 </div>
-                <!-- /.row -->
-            </li>
+            </li> --}}
             <!-- Menu Footer-->
             <li class="user-footer">
                 <a href="#" class="btn btn-default btn-flat">Profile</a>
