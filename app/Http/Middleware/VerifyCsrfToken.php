5e981cecddaecfce880f7fb2f1fd5946b8ab6a8d
vhaf123 <victor.aranaf92@gmail.com>
2020-06-16 12:20:49 -0500

    aumento tiempo sesion

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         'logout',
-        'course-status/*'
+        'course-status/*',
+        'admin/temas/*'
     ];
 }
