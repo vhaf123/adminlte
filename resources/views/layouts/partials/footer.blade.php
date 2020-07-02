86b0d3abd8b9a0ec4573d33a2c73f2c3f8fde788
vhaf123 <victor.aranaf92@gmail.com>
2020-07-02 17:57:00 -0500

    trabajo de seo

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -6,7 +6,7 @@
             <img src="{{asset('img/layouts/logo.svg')}}" alt="" height="30px">
         </a>
 
-        <ul class="d-flex list-unstyled mb-0">
+        {{-- <ul class="d-flex list-unstyled mb-0">
             <li class="mr-2">
                 <a class="text-info" href="{{route('politicas.index')}}">Políticas y privacidad</a>
             </li>
@@ -14,8 +14,9 @@
             <li>
                 <a class="text-info" href="{{route('condiciones.index')}}">Terminos y condiciones</a>
             </li>
-        </ul>
+        </ul> --}}
         
+        <p class="mb-0 text-secondary">Copyright © 2014-2019 <span class="text-info font-weight-bold">Coders Free</span></p>
         
     </div>
 
