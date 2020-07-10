a99a58b6d820c081510aeedcb9f5e6b940184569
vhaf123 <victor.aranaf92@gmail.com>
2020-07-10 13:38:39 -0500

    Seo

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,8 +26,8 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
         /* $schedule->command('sitemap:generate')->daily(); */
-        /* $schedule->command('sitemap:generate')->daily(); */
-        $schedule->command('sitemap:generate')->everyMinute();
+        $schedule->command('sitemap:generate')->daily();
+        /* $schedule->command('sitemap:generate')->everyMinute(); */
     }
 
     /**
